ea8a5701ba09a86f242809a3b3f4f168781bd18b
chuyii <contact@chuyii.net>
2019-12-02 11:31:50 +0900

    [update] アタッチしたスタートボタンによるシーン遷移に修正

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -1,26 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
-    void OnGUI()
+    public Button m_StartButton;
+
+    void Start()
     {
-        Util.SetFontSize(32);
+        Button startButton = m_StartButton.GetComponent<Button>();
 
-        Util.SetFontAlignment(TextAnchor.MiddleCenter);
+        startButton.onClick.AddListener(StartButtonOnClick);
+    }
 
-        float w = 128;
-        float h = 32;
-        float px = Screen.width / 2 - w / 2;
-        float py = Screen.height / 2 - h / 2;
-
-        Util.GUILabel(px, py, w, h, "心中すごろく");
-
-        py += 60;
-        if (GUI.Button(new Rect(px, py, w, h), "START"))
-        {
-            Application.LoadLevel("SimpleStageScene");
-        }
+    void StartButtonOnClick()
+    {
+        SceneManager.LoadScene("SimpleStageScene");
     }
 }
