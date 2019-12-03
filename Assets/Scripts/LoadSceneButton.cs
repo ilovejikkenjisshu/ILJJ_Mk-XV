dcf718eae424ab8a276610a1e184e664b6ac5a5b
chuyii <contact@chuyii.net>
2019-12-03 09:50:46 +0900

    [add] added "LoadSceneButton.cs"

---
+++ Assets/Scripts/LoadSceneButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneButton : MonoBehaviour
+{
+    public string SceneName;
+
+    public void LoadTargetScene() {
+      SceneManager.LoadScene(SceneName);
+    }
+}
