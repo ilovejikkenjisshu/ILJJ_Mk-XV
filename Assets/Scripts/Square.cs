4571870e0885c5bba9160955908197e4c15433e8
Semi11 <stand.say.me@gmail.com>
2019-12-05 16:59:47 +0900

    [fix] Assets/Scripts/Square.cs
    Co-Authored-By: Shotaro Kitamura <shot.kit.garact@gmail.com>

--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -14,7 +14,7 @@ public abstract class Square : MonoBehaviour
             GameObject arrow = Instantiate(guideArrow,transform.position, Quaternion.identity);
             Vector3 diff = (s.transform.position - arrow.transform.position);
             arrow.transform.rotation = Quaternion.FromToRotation (Vector3.up, diff);
-            arrow.transform.position=Vector3.MoveTowards(arrow.transform.position,s.transform.position,1f);
+            arrow.transform.position = Vector3.MoveTowards(arrow.transform.position, s.transform.position, 1f);
         }
     }
 }
