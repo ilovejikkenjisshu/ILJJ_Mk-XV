1c332156d8a45be008f18ec4ff57d77e2e71fa5d
semi11 <stand.say.me@gmail.com>
2019-12-05 17:11:37 +0900

    [fix] change variable name

--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -9,12 +9,12 @@ public abstract class Square : MonoBehaviour
 
     protected virtual void Start()
     {
-        foreach(Square s in GetNext())
+        foreach(Square sqr in GetNext())
         {
             GameObject arrow = Instantiate(guideArrow,transform.position, Quaternion.identity);
-            Vector3 diff = (s.transform.position - arrow.transform.position);
+            Vector3 diff = (sqr.transform.position - arrow.transform.position);
             arrow.transform.rotation = Quaternion.FromToRotation (Vector3.up, diff);
-            arrow.transform.position=Vector3.MoveTowards(arrow.transform.position,s.transform.position,1f);
+            arrow.transform.position=Vector3.MoveTowards(arrow.transform.position,sqr.transform.position,1f);
         }
     }
 }
