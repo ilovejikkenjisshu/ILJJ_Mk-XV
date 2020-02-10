574b250787438857743a2400c69fec60b417948f
proelbtn <proelbtn@users.noreply.github.com>
2020-02-10 15:14:22 +0900

    [update] rotate light for creepy

---
+++ Assets/Scripts/DirectionalLightManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalLightManager : MonoBehaviour
+{
+    float t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Mathf.Sin(t);
+        float y = Mathf.Cos(t);
+
+        Vector3 pos = new Vector3(x, y, -10);
+        gameObject.transform.position = pos;
+        gameObject.transform.LookAt(new Vector3(0, 0, 8f));
+
+        t += 0.01f;
+    }
+}
