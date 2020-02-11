1cfbbf16c08b1911df5ad58fcd108ec62cd0284f
GarAcT <shot.kit.garact@gmail.com>
2020-02-11 14:42:38 +0900

    [add] players and InitPlayers()
    これに伴いSceneからPlayerを消した

--- Assets/Scripts/SimpleStageManager.cs
+++ Assets/Scripts/SimpleStageManager.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class SimpleStageManager : MonoBehaviour, Stage
 {
     public GameObject readyPanel;
+    public Player playerPrefab;
+
+    private Player[] players;
 
     Player[] Stage.GetPlayers()
     {
@@ -20,9 +23,20 @@ public class SimpleStageManager : MonoBehaviour, Stage
     // Start is called before the first frame update
     void Start()
     {
+        InitPlayers(4);
         GameManager manager = new GameManager(this);
         StartCoroutine(manager.Run());
         Debug.Log("Game Initialization finished");
+    }
+
+    // 仮に生成しているだけでPlayerに必要な情報は用意されていない
+    private void InitPlayers(int playerNum)
+    {
+        players = new Player[playerNum];
+        for(int i = 0; i < playerNum; i++)
+        {
+            players[i] = Instantiate(playerPrefab);
+        }
     }
 
     public IEnumerator WaitForGettingReady()
