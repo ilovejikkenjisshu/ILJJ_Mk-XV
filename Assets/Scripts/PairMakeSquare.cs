9e626fe63bbdd2feac542d8928f0278b70b0bbc8
semi11 <stand.say.me@gmail.com>
2020-02-17 11:18:47 +0900

    [fix] ペア作成マスの動作を修正

--- Assets/Scripts/PairMakeSquare.cs
+++ Assets/Scripts/PairMakeSquare.cs
@@ -19,12 +19,17 @@ public class PairMakeSquare : Square
     public override IEnumerator execEvent(Stage stage,int playerNum)
     {
         List<Player> players = stage.GetPlayers();
+
+        if(players[playerNum].Pair!=null)yield break;
+
         IEnumerator rollDice = stage.RollDice(1, 6);
         yield return rollDice;
         int dicenum = (int)rollDice.Current;
         if(dicenum>3){
-            int pairPlayerNum = UnityEngine.Random.Range(0,players.Count);
+            int pairPlayerNum = UnityEngine.Random.Range(0,players.Count-1);
+            if(pairPlayerNum >= playerNum)pairPlayerNum++;
             players[playerNum].Pair = players[pairPlayerNum];
+            players[pairPlayerNum].Pair = players[playerNum];
             Debug.Log("player" + playerNum.ToString() + " paired with player" + pairPlayerNum.ToString());
         }else{
             Debug.Log("player" + playerNum.ToString() + " was not able to form a pair");
